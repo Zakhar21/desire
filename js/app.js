0751a472f816157a156c29009329b461dfd4cdf3
Zakhar21 <ip609108@gmail.com>
2022-04-17 22:47:09 +0700

    sidebar

---
+++ js/app.js
@@ -0,0 +1,6 @@
+$('.header__btn').on('click', function(){
+    $('.rightside-menu').removeClass('rightside-menu--close')
+});
+$('.rightside-menu__close').on('click', function(){
+    $('.rightside-menu').addClass('rightside-menu--close')
+});
