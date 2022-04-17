f674ad8cdb9cd9b20063523f217b397e40dc02f6
Zakhar21 <ip609108@gmail.com>
2022-04-17 23:06:31 +0700

    Главный слайдер start

--- js/app.js
+++ js/app.js
@@ -4,3 +4,9 @@ $('.header__btn').on('click', function(){
 $('.rightside-menu__close').on('click', function(){
     $('.rightside-menu').addClass('rightside-menu--close')
 });
+// Слайдер top
+$('.top__slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
