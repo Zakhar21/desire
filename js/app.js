d7e3a1881ef70b05ea8b24cd5490d81c282dcdb5
Zakhar21 <ip609108@gmail.com>
2022-04-18 08:20:43 +0700

    slixk slider

--- js/app.js
+++ js/app.js
@@ -6,7 +6,11 @@ $('.rightside-menu__close').on('click', function(){
 });
 // Слайдер top
 $('.top__slider').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1
+    // infinite: true,
+    // slidesToShow: 1,
+    // slidesToScroll: 1,
+    dots:true,
+    arrows:false,
+    fade:true,
+    autoplay:true,
 });
